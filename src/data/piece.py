f92c187908ff97b68bed585cd8abf246e33a32a7
Israel Cohen <josh50170@gmail.com>
2025-01-09 17:54:33 +0200

    skeleton: Piece: Add fields to Piece class and fix the inheritance of King class
    Signed-off-by: Israel Cohen <josh50170@gmail.com>

--- src/data/piece.py
+++ src/data/piece.py
@@ -1,18 +1,23 @@
 from abc import ABC, abstractmethod
 
 from src.data.location import Location
+from src.data.enumes import Colors
 
 class Piece(ABC):
+    def __init__(self, color: Colors, location: Location):
+        self.color_player: Colors = color
+        self.location_layer: Location = location
+
     @abstractmethod
-    def valid_steps(self, pos: Location):
+    def valid_steps(self) -> list[Location | list[Location]]:
         pass
 
 
 class Pawn(Piece):
-    def valid_steps(self, pos: Location):
+    def valid_steps(self) -> list[Location | list[Location]]:
         pass
 
 
-class King(Piece):
-    def valid_steps(self, pos: Location):
+class King(Pawn):
+    def valid_steps(self) -> list[Location | list[Location]]:
         pass
